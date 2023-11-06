0367c978dcd6c23d589386d8745c2d3dfc02d7ad
Tian Lechen <lechen.tian@gmail.com>
2023-11-06 16:12:00 +0800

    udpate data basic info

--- dataBasicInfo.py
+++ dataBasicInfo.py
@@ -3,8 +3,13 @@
 import pandas as pd
 
 df = pd.read_csv('women_basket_world_cup_2022_all_players.csv')
+pd.set_option('display.max_columns', None)
+
+print("-------- print this dataframe basic info --------")
 print(df.describe())
-# #pd.set_option('display.max_columns', None)
-# pointTotalDf = df.sort_values(by=['PointsTotal'], ascending=False)
-# pointTotalDf = pointTotalDf[["Team", "OfficialName","PointsTotal"]]
-# print(pointTotalDf.head(10))
+
+print("-------- print this dataframe first row --------")
+print(df.loc[0])
+
+print("-------- print this dataframe 'PointsTotal' column--------")
+print(df['PointsTotal'])
