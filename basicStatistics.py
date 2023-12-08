75773d21e0fbbdd059fd2e9c40c3f381c48d2c3f
Tian Lechen <lechen.tian@gmail.com>
2023-12-08 14:53:25 +0800

    update basic statistics

--- basicStatistics.py
+++ basicStatistics.py
@@ -5,3 +5,6 @@ with open('women_basket_world_cup_2022_all_players.json') as json_file:
 for dat in data:
     print(type(dat))
     print(dat)
+
+
+# basic statistics https://fanlv.fun/2018/06/19/statistics/
