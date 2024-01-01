9cd6ed4c3cc3dd5a40fddfa2f59164f8b12c6fe7
lechentian <lechen.tian@gmail.com>
2024-01-01 18:56:03 +0800

    explanation of the graph--top ten players by efficiency

--- README.md
+++ README.md
@@ -1,20 +1,27 @@
 # Women Basket World Cup 2022 - All Players Data Analysis
 
-## 1 get and save all players data to local
+## 1 Get and save all players data to local
     https://www.fiba.basketball/womensbasketballworldcup/2022/playerstats
   python script : getPlayersData.py save output as 'women_basket_world_cup_2022_all_players.json'  
-  
 
 
-## 2 preprocessing data
+## 2 Preprocessing data
    download file is json file, to use pandas library, we need convert json file to csv.\
    we need the following change for convert data.\
    1 add new column as Team.\
    2 add new column 'ReboundsTotal',get value from DefensiveReboundsTotal and OffensiveReboundsTotal
 
 ## 3 Player data analysis
-### 3.1 top 10 player by efficiency 
+### 3.1 Top 10 players by efficiency 
 ![top 10 player by Efficiency](chart/top10playerByEfficiency.svg)
+
+The graph is created based on the efficiency of the players. Efficiency is one of the most important critera to 
+test a player's value, and it is culculated by a formula, which contains many different data from multiple aspects, 
+so I chose this index to create the graph. Base on the graph, there are several conclusions we can get.\
+First, most directly, we can see the ranking of the players based on efficiency and who were the top ten. \
+Second, although the top one player has the highest efficiency, not all of her data is the top one of the game. For example, 
+the total points her got was not the highest. This further reflects that efficiency is a comprehensive index.
+
 
 ### 3.2 Player Compare 
 Top 5 China Player Compare
--- showTop10PlayersByEfficiency.py
+++ showTop10PlayersByEfficiency.py
@@ -4,18 +4,21 @@ import pygal
 
 df = pd.read_csv('women_basket_world_cup_2022_all_players.csv')
 efficiencyDf = df.sort_values(by=['Efficiency'], ascending=False)
-efficiencyDf = efficiencyDf[["Team", "OfficialName","Efficiency","PointsTotal","AssistsTotal","DefensiveReboundsTotal",
-                             "StealsTotal", "TurnOversTotal", "PersonalFoulsTotal"]]
-#print(efficiencyDf.head(10)["OfficialName"].tolist())
-#print(efficiencyDf.head(10)["Efficiency"].tolist())
+
+efficiencyDf = efficiencyDf[["Team", "OfficialName","Efficiency","PointsTotal","MinutesTotal","AssistsTotal",
+                             "ReboundsTotal","StealsTotal","TurnOversTotal"]]
+# print(efficiencyDf)
+# print(efficiencyDf.head(10)["OfficialName"].tolist())
+# print(efficiencyDf.head(10)["Efficiency"].tolist())
 
 bar_chart = pygal.Bar()
-bar_chart.title = 'Top 10 Player by Efficiency'
+bar_chart.title = 'Top 10 Players by Efficiency'
 bar_chart.x_labels = efficiencyDf.head(10)["OfficialName"].tolist()
 bar_chart.add('Efficiency', efficiencyDf.head(10)["Efficiency"].tolist())
 bar_chart.add('Points', efficiencyDf.head(10)["PointsTotal"].tolist())
 bar_chart.add('Assist',      efficiencyDf.head(10)["AssistsTotal"].tolist())
 bar_chart.add('Steal',  efficiencyDf.head(10)["StealsTotal"].tolist())
 bar_chart.add('Turn Overs',  efficiencyDf.head(10)["TurnOversTotal"].tolist())
-bar_chart.add('Fouls',  efficiencyDf.head(10)["PersonalFoulsTotal"].tolist())
+bar_chart.add('Rebounds',  efficiencyDf.head(10)["ReboundsTotal"].tolist())
+bar_chart.add('Minutes',  efficiencyDf.head(10)["MinutesTotal"].tolist())
 bar_chart.render_to_file("chart/top10playerByEfficiency.svg")
