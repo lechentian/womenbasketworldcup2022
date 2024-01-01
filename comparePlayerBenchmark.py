a0f496506796899fb2c1ba37cb852e5d4ed850bc
lechentian <lechen.tian@gmail.com>
2024-01-01 19:48:46 +0800

    added new index in the compare players' benchmark diagram

--- comparePlayerBenchmark.py
+++ comparePlayerBenchmark.py
@@ -6,15 +6,13 @@ df = pd.read_csv('women_basket_world_cup_2022_all_players.csv')
 # print(df.describe())
 pd.set_option('display.max_columns', None)
 #topEfficiencyPlayerDf = df.sort_values(by=['Efficiency'], ascending=False)
-topEfficiencyPlayerDf = df[["PlayerId","Team", "OfficialName", "PointsTotal", "AssistsTotal", "FreeThrowsMadeTotal",
-     "OffensiveReboundsTotal", "DefensiveReboundsTotal", "StealsTotal"]]
+topEfficiencyPlayerDf = df[["PlayerId","Team", "OfficialName", "PointsTotal", "AssistsTotal", "FreeThrowsMadeTotal", "ReboundsTotal", "TurnOversTotal", "StealsTotal"]]
 #topEfficiencyPlayerDf = topEfficiencyPlayerDf.head(10)
 #print(topEfficiencyPlayerDf)
 
-topEfficiencyPlayerDf[["PointsTotal", "AssistsTotal", "FreeThrowsMadeTotal",
-                       "OffensiveReboundsTotal", "DefensiveReboundsTotal","StealsTotal"]] \
+topEfficiencyPlayerDf[["PointsTotal", "AssistsTotal", "FreeThrowsMadeTotal","ReboundsTotal", "TurnOversTotal","StealsTotal"]] \
 = MinMaxScaler().fit_transform(topEfficiencyPlayerDf[["PointsTotal", "AssistsTotal", "FreeThrowsMadeTotal",
-                           "OffensiveReboundsTotal", "DefensiveReboundsTotal", "StealsTotal"]])
+                           "ReboundsTotal", "TurnOversTotal", "StealsTotal"]])
 #print(topEfficiencyPlayerDf)
 
 
@@ -34,7 +32,7 @@ print(secondPlayerList)
 
 radar_chart = pygal.Radar()
 radar_chart.title = 'Player benchmark results'
-radar_chart.x_labels = ['Points', 'Assists', 'FreeThrows', 'OffensiveRebounds', 'DefensiveRebounds', 'Steals']
+radar_chart.x_labels = ['Points', 'Assists', 'FreeThrows', 'Rebounds', 'TurnOvers', 'Steals']
 radar_chart.add(firstPlayerList[2], firstPlayerList[3:])
 radar_chart.add(secondPlayerList[2], secondPlayerList[3:])
 radar_chart.add(thirdPlayerList[2], thirdPlayerList[3:])
